6e514e2d13a3bf988b0b68565376b2cbab1ddcaa
pablo <pablo.rosales14@gmail.com>
2019-01-22 10:20:10 -0800

    initial commit

---
+++ src/main/java/com/pablorosales/healthtrackerbackend/HealthTrackerBackendApplication.java
@@ -0,0 +1,14 @@
+package com.pablorosales.healthtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthTrackerBackendApplication.class, args);
+	}
+
+}
+
