84f787a0b911b573abec4e28336ed87b082887d1
pablo <pablo.rosales14@gmail.com>
2019-01-23 13:25:45 -0800

    Trying to make a server from memory... not working that well

--- src/main/java/com/pablorosales/healthtrackerbackend/ExerciseController.java
+++ src/main/java/com/pablorosales/healthtrackerbackend/ExerciseController.java
@@ -1,20 +1,36 @@
 package com.pablorosales.healthtrackerbackend;
 
+
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 
-@Controller
+@RestController
 public class ExerciseController {
 
 @Autowired
 private ExerciseRepository exerciseRepository;
 
-@PostMapping(value="/exercises")
-public void saveExercise(){
+  @PostMapping(value = "/exercises")
+  public void postExercise(
+          @RequestParam String title,
+          @RequestParam String quantity,
+          @RequestParam String description) {
+            Exercise newExercise = new Exercise(title, quantity, description, new Date().toString());
+            ExerciseRepository.save(newExercise);
+          }
+
+  @GetMapping(value = "/exercises")
+  public String getExercises() {
+    Gson gson = new Gson();
+    String JsonExercise = gson.toJson(ExerciseRepository.findAll());
+    return JsonExercise;
+  }
 
 }
 
-
-}
