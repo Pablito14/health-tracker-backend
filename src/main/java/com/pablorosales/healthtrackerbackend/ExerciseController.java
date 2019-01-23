a0ce07a02f1bffa5083496bd5997a4e49f6cd73a
pablo <pablo.rosales14@gmail.com>
2019-01-23 11:24:19 -0800

    creating the controller

--- src/main/java/com/pablorosales/healthtrackerbackend/ExerciseController.java
+++ src/main/java/com/pablorosales/healthtrackerbackend/ExerciseController.java
@@ -1,11 +1,20 @@
 package com.pablorosales.healthtrackerbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class ExerciseController {
 
+@Autowired
+private ExerciseRepository exerciseRepository;
 
+@PostMapping(value="/exercises")
+public void saveExercise(){
+
+}
 
 
 }
---
+++ src/main/java/com/pablorosales/healthtrackerbackend/ExerciseRepository.java
@@ -0,0 +1,7 @@
+package com.pablorosales.healthtrackerbackend;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
+
+}
