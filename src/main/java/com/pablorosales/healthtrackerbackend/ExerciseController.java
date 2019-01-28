4e509cb29a4fb31b204c0f6e7a39be51c05e8823
pablo <pablo.rosales14@gmail.com>
2019-01-27 20:27:24 -0800

    got the server running, need to make request/response work

--- src/main/java/com/pablorosales/healthtrackerbackend/ExerciseController.java
+++ src/main/java/com/pablorosales/healthtrackerbackend/ExerciseController.java
@@ -1,6 +1,5 @@
 package com.pablorosales.healthtrackerbackend;
 
-
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,28 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
-
 @RestController
 public class ExerciseController {
 
 @Autowired
-private ExerciseRepository exerciseRepository;
+public ExerciseRepository exerciseRepository;
 
   @PostMapping(value = "/exercises")
-  public void postExercise(
-          @RequestParam String title,
-          @RequestParam String quantity,
-          @RequestParam String description) {
-            Exercise newExercise = new Exercise(title, quantity, description, new Date().toString());
-            ExerciseRepository.save(newExercise);
-          }
+  public void postExercise(@RequestParam String title, @RequestParam String quantity, @RequestParam String description) {
+    Exercise newExercise = new Exercise(title, quantity, description);
+    exerciseRepository.save(newExercise);
+  }
 
   @GetMapping(value = "/exercises")
   public String getExercises() {
     Gson gson = new Gson();
-    String JsonExercise = gson.toJson(ExerciseRepository.findAll());
-    return JsonExercise;
+    return gson.toJson(exerciseRepository.findAll());
   }
 
 }
-
--- src/main/java/com/pablorosales/healthtrackerbackend/ExerciseRepository.java
+++ src/main/java/com/pablorosales/healthtrackerbackend/ExerciseRepository.java
@@ -2,6 +2,4 @@ package com.pablorosales.healthtrackerbackend;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
-
-}
+public interface ExerciseRepository extends CrudRepository<Exercise, Long> {}
